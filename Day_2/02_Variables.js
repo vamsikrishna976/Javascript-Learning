9f0857e187ae9cdb235145679931151495ad77c7
vamsikrishna976 <paluruvamsikrishna16@gmail.com>
2025-01-13 22:30:40 +0530

    Refactor: restructure project by moving Day 1 files to Day_1 and adding new implementations

---
+++ Day_2/02_Variables.js
@@ -0,0 +1,18 @@
+let a = 45
+console.log(a)
+
+a=53
+console.log(a)
+
+a=5367
+console.log(a)
+
+a="harry"
+console.log(a)
+
+var b = 45
+console.log(b)
+
+var b="Johny"
+var c = b +" is a web developer"
+console.log(c)
