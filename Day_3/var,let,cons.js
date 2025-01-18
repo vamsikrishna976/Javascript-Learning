89c2a20b6753b7b1c4537973a0241f30e67f2e26
vamsikrishna976 <paluruvamsikrishna16@gmail.com>
2025-01-18 20:45:36 +0530

    Add Day 3 and Day 4 implementations with HTML and JavaScript files for data types and variable usage

---
+++ Day_3/var,let,cons.js
@@ -0,0 +1,20 @@
+console.log("Javascript tutorial day 3")
+// Using var keyword //
+var num
+num = 34
+console.log(num)
+num = 45
+console.log(num)
+var num = 533
+console.log(num)
+
+// Using let keyword // 
+let num1 = 34
+console.log(num1)
+let num2 = 45
+console.log(typeof(num1))
+
+ // Using const keyword //
+ const num3 = 34
+ console.log(num3)
+ 
---
+++ Day_4/DataTypes.js
@@ -0,0 +1,34 @@
+               // DATA TYPES IN JAVASCRIPT
+
+           // Primitive Data Types in Javascript
+var a = 18
+console.log(typeof a) // number
+
+let name1 = "Vamsi"
+console.log(typeof name1) // string
+
+const b = true
+console.log(typeof b) // boolean
+
+         // Special Data Types in Javascript
+let c = null
+console.log(typeof c) // object 
+
+let d = undefined
+console.log(typeof d) // undefined
+
+let e ;
+console.log(typeof e) // undefined
+
+         // Compositive Data Types in Javascript
+let marks = [34, 67, 89, 56, 78, 90] // Array
+console.log(marks) 
+
+
+let student = {
+    name: "Vamsi",
+    age: 18,
+    Married : false,
+} 
+console.log(student)
+console.log(typeof(student))
