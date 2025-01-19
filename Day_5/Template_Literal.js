87d70c8d140948048bed2f4baf5866bec8876fb3
vamsikrishna976 <paluruvamsikrishna16@gmail.com>
2025-01-19 13:54:38 +0530

    Add Day 5 implementation with Template Literal examples in JavaScript and corresponding HTML file

---
+++ Day_5/Template_Literal.js
@@ -0,0 +1,19 @@
+// Without Template Literal
+var a =" to ";
+console.log(("welcome" + a + "javascript"));
+
+console.log("welcome to \nmy world"); // welcome to
+                                      // my world                 
+
+// With Template Literal
+var b = "a Web developer";
+console.log(`Hey hi i am vamsi ${b} Welcome to my world`); // Hey hi i am vamsi i am a  Web developer
+
+let x = 20;
+let y = 60;
+console.log(`sum of ${x} and ${y} : ${x+y}`)
+console.log(`${x+y}`)
+
+console.log(`Welcome to 
+my world`) // Welcome to
+           // my world       
