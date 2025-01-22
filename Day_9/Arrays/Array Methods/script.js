b57454ec4a7f74cfaf5546486d887f8df36aa357
vamsikrishna976 <paluruvamsikrishna16@gmail.com>
2025-01-22 12:11:18 +0530

    Add Day 9 implementations for Array Methods in HTML and JavaScript

---
+++ Day_9/Arrays/Array Methods/script.js
@@ -0,0 +1,28 @@
+//Methods in array
+var fruits = ['apple', 'banana', 'guava'];
+console.log(fruits);
+document.write(fruits);
+//push method 
+fruits.push('pine apple');
+console.log(fruits);
+//Pop Method
+ console.log(fruits.pop());
+ console.log(fruits);
+//unshift Method
+fruits.unshift('papaya');
+console.log(fruits);
+//Shift Method
+console.log(fruits.shift())
+console.log(fruits);
+//Index of () Method
+var fruits1= ['apple', 'banana', 'guava', 'pineapple'];
+console.log(fruits1.indexOf("banana"));
+//Slice Method 
+console.log(fruits1.slice(0,6));
+
+
+
+
+
+
+
---
+++ Day_9/Arrays/script.js
@@ -0,0 +1,25 @@
+//Array 
+//Syntax
+//var arr = new Array(element0, element1, ..., elementN);
+//var arr = [element0, element1, ..., elementN];
+//Example
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+//Output
+//1, 2, 3, 4, 5
+
+var arr = ["vamsi", "krishna", "paluru", "sukanya", "praveen"]
+console.log([0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+arr [5] = "kumar"
+console.log(arr);
+console.log(arr.length);
+// Output
+// vamsi
+// krishna
+// paluru
+// sukanya
+// praveen
