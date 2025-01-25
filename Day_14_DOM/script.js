f98163de26a1612a9af481574b02111d2bba4451
vamsikrishna976 <paluruvamsikrishna16@gmail.com>
2025-01-25 22:07:56 +0530

    Add new HTML and JavaScript files for Day 14 DOM implementation; set background colors and log elements

---
+++ Day_14_DOM/script.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundColor = 'lightblue';
+var x= document.getElementById("Style");
+console.log(x);
+document.getElementsByClassName("style1").style.backgroundColor = 'red';
